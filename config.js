88db1f2911058f293b4d5444031a655c0e7f643a
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-10-25 15:20:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
-SUDO_NB: process.env.SUDO_NB || "94767910958",
+SESSION_ID: process.env.SESSION_ID || "DId1QayS#szwnz9YoB7DyuRZ6_xC5UcK3kIvAEmQr6zefezlDnTc",
+SUDO_NB: process.env.SUDO_NB || "94711451319",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "false",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "unavailable",
