f8111859aeac4958d8aef283123cd622f1e30cb0
DILISHA-LK11 <dgimshan101@gmail.com>
2025-01-12 20:54:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DId1QayS#szwnz9YoB7DyuRZ6_xC5UcK3kIvAEmQr6zefezlDnTc",
+SESSION_ID: process.env.SESSION_ID || "77whERZB#fEeEavbcYfyA_f1UAY3uEkhK95WUp89BhNZ2nOuevAw",
 SUDO_NB: process.env.SUDO_NB || "94711451319",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "false",
